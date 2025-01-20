cc480cf7ce554d8489b373b5e79d19946bc5f396
JosueGualotuna <mateogjosuasim@gmail.com>
2025-01-20 10:55:49 -0500

    Add files via upload

---
+++ PryNavegacion/src/prynavegacion/PryNavegacion.java
@@ -0,0 +1,14 @@
+
+package prynavegacion;
+
+import vista.PantallaPrincipal;
+
+public class PryNavegacion {
+
+    
+    public static void main(String[] args) {
+        PantallaPrincipal PantallaPrincipal1 = new PantallaPrincipal();
+        PantallaPrincipal1.setVisible(true);
+    }
+    
+}
---
+++ PryNavegacion2/src/prynavegacion2/PryNavegacion2.java
@@ -0,0 +1,18 @@
+
+
+package prynavegacion2;
+
+import vista.PantallaPrincipal;
+
+
+public class PryNavegacion2 {
+
+
+   
+    public static void main(String[] args) {
+        PantallaPrincipal principal = new PantallaPrincipal();
+        principal.setVisible(true);
+        
+    }
+    
+}
