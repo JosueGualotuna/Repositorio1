accee4c82e9e6106aa5aaca1dd5a3554d9dc8ebf
JosueGualotuna <mateogjosuasim@gmail.com>
2025-01-19 22:26:57 -0500

    Add files via upload

---
+++ datospersonaConEdad/PryDatosPersona1/src/prydatospersona1/PryDatosPersona1.java
@@ -0,0 +1,14 @@
+
+package prydatospersona1;
+
+import vista.datosVista;
+
+public class PryDatosPersona1 {
+
+    public static void main(String[] args) {
+        datosVista vista = new datosVista();
+        vista.setVisible(true);
+        
+    }
+    
+}
