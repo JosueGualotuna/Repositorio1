102145f768d9a163860941c4fdfa2ac396945d1a
JosueGualotuna <mateogjosuasim@gmail.com>
2025-01-23 23:16:00 -0500

    Add files via upload

---
+++ CalculadoraInterfaz/src/CalculadoraInterfaz/CalculadoraInterfaz.java
@@ -0,0 +1,11 @@
+
+package CalculadoraInterfaz;
+
+public class CalculadoraInterfaz {
+    public static void main(String[] args) {
+        calculadoraVista pantalla1 = new calculadoraVista();
+     pantalla1.setVisible(true);
+    pantalla1.setLocationRelativeTo(null);
+    }
+    
+}
